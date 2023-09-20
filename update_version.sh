396406763d56f2257e2b843241f73521d15841a2
imwaitingnow <alan.marshall987@gmail.com>
2023-09-20 19:31:38 +0100

    auto list update

---
+++ update_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $(git diff --name-only HEAD^ supported/games supported/appids) ]]; then
+  current_version=$(<supported/list_version)
+  new_version=$((current_version + 1))
+  echo $new_version > supported/list_version
+  git add supported/list_version
+  git commit -m "Incremented list version to $new_version"
+  git push
+fi

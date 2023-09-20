ee4bbd2b37c451d3f82946f4ec5ef7e4a4958df8
imwaitingnow <38100920+imwaitingnow@users.noreply.github.com>
2023-09-20 19:42:25 +0100

    Update update_version.sh

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create the files if they don't exist
+touch supported/games
+touch supported/appids
+touch supported/list_version
+
 if [[ $(git diff --name-only $GITHUB_SHA..HEAD supported/games supported/appids) ]]; then
   current_version=$(<supported/list_version)
   new_version=$((current_version + 1))
